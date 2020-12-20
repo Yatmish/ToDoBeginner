3fa4841a2975deae35a7fccbd7b9aecc97f1d78a
Gabriel Trigueiro <gabriel.trigueiromelo@gmail.com>
2020-12-20 13:56:36 -0300

    deletebtn

--- js/main.js
+++ js/main.js
@@ -7,13 +7,28 @@ function createToDo(event) {
         let mainDiv = document.createElement('div')
         let toDo = document.createElement("INPUT")
         let toDoMsg = document.createElement('label')
+        let redTrash = document.createElement('img')
+
         
+
         toDoMsg.classList.add("todoMsg")
+        
         toDoMsg.innerHTML = todoText.value 
         
+        redTrash.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fdownload%2Fsoylent-red%2Ftrash-2-48.png&f=1&nofb=1'
+        
+        redTrash.id = 'deleteTrash'
+        
+        redTrash.alt = 'red trash'
+        
+        redTrash.addEventListener('click', () => {
+            mainDiv.remove()
+        })
+
         mainDiv.classList.add('toDo')
 
         toDo.classList.add('todo')
+       
         toDo.type = 'checkbox'
         toDo.addEventListener('click', () => {
             if(toDo.checked) {
@@ -24,9 +39,13 @@ function createToDo(event) {
         })
 
         todoText.value = '';
-
+        
         sessionToDo.appendChild(mainDiv);
+        
         mainDiv.appendChild(toDo);
-        mainDiv.appendChild(toDoMsg)
+        
+        mainDiv.appendChild(toDoMsg);
+        
+        mainDiv.appendChild(redTrash);
     }
 }
